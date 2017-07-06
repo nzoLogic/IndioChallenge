50feefeddc2dbceebfe43be2e92affc223b2d4d3
nzoLogic <nzoLogic@gmail.com>
2017-07-06 10:35:49 -0700

    changed #checkStorage to return an empty array if localStorage is empty

--- src/Local.js
+++ src/Local.js
@@ -1,14 +1,7 @@
 import React from 'react'
 
-const Questions = {
-  q: null,
-  type: null,
-  subq: null
-}
-
 const checkStorage = (key) => {
-  let questions = window.localStorage.getItem(key) || Questions
-  return questions 
+  return window.localStorage.getItem(key) || [] 
 }
 
 export { checkStorage }
