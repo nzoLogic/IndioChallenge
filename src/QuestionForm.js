1546241ebdccfda8c0ae34017cef0b30b98998ce
nzoLogic <nzoLogic@gmail.com>
2017-07-06 13:03:04 -0700

    changed Form/Input components to classes for updating their own states

--- src/QuestionForm.js
+++ src/QuestionForm.js
@@ -2,14 +2,20 @@ import React, { Component } from 'react'
 import { Form, Input, Container } from 'semantic-ui-react'
 import QuestionInput from './QuestionInput.js'
 
-const QuestionForm = (props) => {
-  const questions = [{ q: 'What would you like?', type: 'text', subQ: null}]
-  return(
-    <Container>
-    <Form>
-      {questions.map( (q, i) => <QuestionInput key={i} question={q} /> )}
-    </Form>
-    </Container>
-  )
+class QuestionForm extends Component {
+  constructor(props){
+    super(props)
+  }
+  render(){
+    const questions = [{ q: 'What would you like?', type: 'text', subQ: null}]
+    return(
+      <Container>
+        <Form>
+          {questions.map( (q, i) => <QuestionInput key={i} question={q} /> )}
+        </Form>
+      </Container>
+    )
+  }
+  
 }
 export default QuestionForm
