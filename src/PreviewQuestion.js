ea1428e6d85402b5011a0713d0421161aea8ab0e
nzoLogic <nzoLogic@gmail.com>
2017-07-12 11:15:18 -0700

    condition questions rendering

--- src/PreviewQuestion.js
+++ src/PreviewQuestion.js
@@ -11,33 +11,31 @@ class PreviewQuestion extends Component {
     }
     this.input = this.input.bind(this)
     this.radio = this.radio.bind(this)
-    this.updateAnswer = this.updateAnswer.bind(this)
+    this.handleInputChange = this.handleInputChange.bind(this)
     this.handleAnswer = this.handleAnswer.bind(this)
     this.handleRef = this.handleRef.bind(this)
-    this.onEnter = this.onEnter.bind(this)
   }
   handleRef(r){
     if(r !== null) r.inputRef.onblur = this.handleAnswer
   }
-  updateAnswer(e, { name, value }){
-    console.log(name, value)
-    this.setState({answer: value})
-    this.handleAnswer(value)
+  handleInputChange(e, { name, value }){
+    console.log(e.target.value)
+    this.state.answer = value
+    if(name === 'radio') this.handleAnswer()
   }
-  handleAnswer(){
+  handleAnswer(value){
     const { question } = this.props
-    const { subQ } = question
+    const { type, subQ } = question
     const { answer } = this.state
     let a = _.find(subQ, q => {
       let { condition, value } = q.conditions
-        if(question.type === 'text' || question.type ==='yn' || condition === 'equals') return answer === value
+      
+        if(type === 'text' || type ==='yn' || condition === 'equals') return answer === value
+        
         if(condition === 'less') return parseInt(answer) < parseInt(value)
         return parseInt(answer) > parseInt(value)
     })
     this.setState({metCondition: a})
-  }
-  onEnter(e){
-    if(e.code === 'Enter') this.handleAnswer()
   }
   input(props){
     return <Input className='answer'
@@ -46,22 +44,20 @@ class PreviewQuestion extends Component {
             name='answer' 
             type={props.question.type} 
             placeholder='answer'
-            onChange={this.updateAnswer}
+            onChange={this.handleInputChange}
             /> 
   }
   radio(){
     return (
       <Form.Group>
-        <Form.Field control={Radio} label='Yes' name='answer' value='yes' checked={this.state.answer==='yes'} onChange={this.updateAnswer}/>
-        <Form.Field control={Radio} control={Radio} label='No' value='no' checked={this.state.answer==='no'} onChange={this.updateAnswer} />
+        <Form.Field control={Radio} type='radio' label='Yes' name='radio' value='yes' checked={this.state.answer==='yes'} onChange={this.handleInputChange}/>
+        <Form.Field control={Radio} type='radio' label='No' name='radio' value='no' checked={this.state.answer==='no'} onChange={this.handleInputChange} />
       </Form.Group>
     )
   }
   render(){
-    const state = { ...this.state }
-    const { answer } = state
-    const { metCondition } = state
-    const question = { ...this.props.question }
+    const { metCondition } = this.state
+    let question = { ...this.props.question }
 
     return(
       <div>
@@ -72,7 +68,8 @@ class PreviewQuestion extends Component {
           </Form.Field>
         </Form.Group>
         
-        { metCondition ? <PreviewQuestion question={metCondition}/>  : null }
+        { metCondition ? 
+          <PreviewQuestion question={metCondition}/>  : null }
       </div>
       )
   }
